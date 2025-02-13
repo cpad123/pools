e743d29b45395bc3d2697a422f8f1243e30e5547
cpad123 <77091708+cpad123@users.noreply.github.com>
2025-02-13 15:57:16 -0500

    Delete as-gen directory

--- as-gen/lib/util.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.capitalize = void 0;
-function capitalize(word) {
-    return word
-        .toLowerCase()
-        .replace(/\w/, firstLetter => firstLetter.toUpperCase());
-}
-exports.capitalize = capitalize;
